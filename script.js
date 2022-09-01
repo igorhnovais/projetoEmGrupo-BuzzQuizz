b1caf4c1f2430a087bd8f1d45a720ae654282c61
erickssguerra <erickssguerra@gmail.com>
2022-08-31 21:42:05 -0300

    Correções do Layout 2.1

--- script.js
+++ script.js
@@ -7,46 +7,46 @@ let myStorage = localStorage;
 
 // Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
-	return Math.random() - 0.5; 
+    return Math.random() - 0.5; 
 }*/
 
 
-function isUserQuiz (id){
-    for (let i = 0; i < localStorage.length; i++){
-        if(localStorage.getItem(localStorage.key(i)) === id)
+function isUserQuiz(id) {
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.getItem(localStorage.key(i)) === id)
             return true;
     }
     return false;
 }
 
-function selectOptionQuiz (ElementClicked){
+function selectOptionQuiz(ElementClicked) {
     let jaSelecionou = false;
     const listOptions = ElementClicked.parentElement.querySelectorAll('.alternativa');
     listOptions.forEach(element => {
-        if(element.classList.contains('selecionado'))
+        if (element.classList.contains('selecionado'))
             jaSelecionou = true;
     });
-    
-    if (!jaSelecionou){
+
+    if (!jaSelecionou) {
         ElementClicked.classList.add('selecionado');
         listOptions.forEach(element => {
-        if(!element.classList.contains('selecionado'))
-            element.classList.add('esfumacado');
+            if (!element.classList.contains('selecionado'))
+                element.classList.add('esfumacado');
         });
     }
-console.log(ElementClicked.parentElement.parentElement.nextElementSibling);
-    setTimeout(function (){
-        if (ElementClicked.parentElement.parentElement.nextElementSibling == null){
+    console.log(ElementClicked.parentElement.parentElement.nextElementSibling);
+    setTimeout(function () {
+        if (ElementClicked.parentElement.parentElement.nextElementSibling == null) {
             resultQuiz();
 
-        } else{
+        } else {
             ElementClicked.parentElement.parentElement.nextElementSibling.scrollIntoView();
         }
 
     }, 2000);
 }
 
-function resultQuiz(){
+function resultQuiz() {
     let hits = 0;
     const boxResultado = document.querySelector('.box-resultado');
     const numberOfQuestions = document.querySelectorAll('.box-pergunta').length;
@@ -56,7 +56,7 @@ function resultQuiz(){
             hits++;
     });
     boxResultado.innerHTML = `
-    <div class="titulo-resultado">${100 * Math.round(hits/numberOfQuestions)}% de acerto: Você é praticamente um aluno de Hogwarts!</div>
+    <div class="titulo-resultado">${100 * Math.round(hits / numberOfQuestions)}% de acerto: Você é praticamente um aluno de Hogwarts!</div>
     <div class="resultado">
         <img src="./imagens/resultado.png" />
         <p>Parabéns Potterhead! Bem-vindx a Hogwarts,
@@ -68,7 +68,7 @@ function resultQuiz(){
         <button class="botao-reiniciar">Reiniciar Quizz</button>
         <button class="botao-home">Voltar pra Home</button>
     </div>`
-    ;
+        ;
     boxResultado.parentElement.classList.remove('escondido');
     boxResultado.scrollIntoView();
 }
@@ -76,27 +76,27 @@ function resultQuiz(){
 /*  requestQuizzes() -> faz a requisição dos quizes disponíveis no servidor e chama a função initQuizzes
  se for bem sucedida; caso contrário, chama a função requestError */
 
- function requestQuizzes(){
+function requestQuizzes() {
     let promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
     promise.then(initQuizzes);
     promise.catch(requestError);
- }
+}
 
 /*  initQuizzes(response) -> carrega o layout 1 do desktop com a lista de quizes do usuário e a lista de todos
  quizes disponíveis extraído do servidor */
 
- function initQuizzes(response){
+function initQuizzes(response) {
     // preenche a seção meus quizzes caso haja algum quiz criado pelo usuário
     const allQuizzes = document.querySelector('.container-quizzes');
     allQuizzes.innerHTML = '';
-    if (myStorage.length != 0){
+    if (myStorage.length != 0) {
         const myQuizzes = document.querySelector('.container-seus-quizzes');
         changeLayout('desktop-1', 'desktop-2');
         myQuizzes.innerHTML = '';
     }
     // preenche os quizzes do servidor
     response.data.forEach(element => {
-        if (isUserQuiz(element.id)){
+        if (isUserQuiz(element.id)) {
             myQuizzes.innerHTML += `
             <div class="box-quizz ${element.id}" onclick="clickedQuiz(${element.id})">
                 <img src= ${element.image} />
@@ -109,19 +109,19 @@ function resultQuiz(){
             <div class="titulo-box-quizz">${element.title}</div>
         </div>`
     });
- 
- }
 
- /* requestError(response) -> envia um alert de qual erro ocorreu durante a requisição ou post*/
+}
 
- function requestError(response){
+/* requestError(response) -> envia um alert de qual erro ocorreu durante a requisição ou post*/
+
+function requestError(response) {
     alert(response.status);
- }
+}
 
 /*  changeLayout(toHideClass, toShowClass) -> recebe duas strings como parâmetros: um referente a 
     classe do layout a ser escondido; e o outro a do layout a ser mostrado */
 
-function changeLayout(toHideClass, toShowClass){
+function changeLayout(toHideClass, toShowClass) {
     const toHideElement = document.querySelector('.' + toHideClass);
     const toShowElement = document.querySelector('.' + toShowClass);
     toHideElement.classList.add('escondido');
@@ -132,17 +132,17 @@ function changeLayout(toHideClass, toShowClass){
 requisição de acesso ao servidor e chama a função displayQuiz se for bem sucedida; caso contrário,
 chama a função requestError  */
 
-function clickedQuiz(idQuizSelected){
+function clickedQuiz(idQuizSelected) {
     let promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizSelected}`);
     promise.then(displayQuiz);
     promise.catch(requestError);
 }
 
 /*  displayQuiz(response) -> mostra o quiz no layout de exibição do quiz para ser respondido    */
-function displayQuiz(response){
+function displayQuiz(response) {
     const layoutShowQuiz = document.querySelector('.desktop-4');
     const quizReceived = response.data;
-    changeLayout('lista-quizzes','pagina-quizz');
+    changeLayout('lista-quizzes', 'pagina-quizz');
     layoutShowQuiz.innerHTML = `
     <div class="titulo-quizz">
           <p>${quizReceived.title}</p>
@@ -158,7 +158,7 @@ function displayQuiz(response){
             ${element.title}
           </div>
           <ul class="alternativas-pergunta">
-        `;
+        `
         element.answers.forEach(answer => {
             layoutShowQuiz.innerHTML += `
             <li class="alternativa ${answer.isCorrectAnswer}" onclick="selectOptionQuiz(this)">
@@ -166,12 +166,12 @@ function displayQuiz(response){
                 <p>${answer.text}</p>
             </li>
             
-            `
+            `;
         });
         layoutShowQuiz.innerHTML += `
           </ul>
         </div>
-        `
+        `;
     });
     console.log(response.data);
 }
@@ -184,11 +184,11 @@ retornando ao estado inicial de exibição do quiz  */
  esvaziando todos os inputs em seguida, e enviando uma requisição de inscrição para o servidor: se for 
  bem sucedido, chama a função subscribedQuiz; caso contrário, chama a função requestError*/
 
- /* subscribedQuiz(response) -> muda para o layout avisando que o quiz está pronto (Desktop - 11) */
+/* subscribedQuiz(response) -> muda para o layout avisando que o quiz está pronto (Desktop - 11) */
 
 
- 
- 
+
+
 //Alguns Detalhes de implementação
 
 // Usar Local Storage pra armazenar os ids dos quizzes do usuário
@@ -201,7 +201,7 @@ retornando ao estado inicial de exibição do quiz  */
 
 // Não esquecer os atributos p/ correção automática
 
- //Seção bônus (a ver)
+//Seção bônus (a ver)
 
 
- requestQuizzes();
+requestQuizzes();
